d9cf9ce607b203be3e314d4984d01968e67511b7
League Student <league@iMac-6.local>
2019-06-02 17:29:46 -0700

    start bit shifter

--- src/_01_Bit_Shifting/BitShifter.java
+++ src/_01_Bit_Shifting/BitShifter.java
@@ -4,11 +4,23 @@ public class BitShifter {
 	public static void main(String[] args) {
 		// 1. Jot down the value of num in binary.
 		int num = 4;
-		int numShifted = num << 0;
+		int numShifted = num << 3;
 		
 		// 2. Print the value of numShifted, and convert that value to binary.
-		
+
+//		String str = "";
+//		int one = 1;
+//		int n = 0;
+//		while(n < num) {
+//			str = "" + 2*one + "" + one;
+//			n = Integer.parseInt(str);
+//		}
+//		System.out.println(str);
 		// 3. Compare the two binary values. Can you figure out what the << operator is for?
+		
+		//shifts each bit 3 places to the left
+		// >> shifts each bit 3 places to right
+		System.out.println(numShifted);
 		
 		// 4. Try shifting num 3 places.
 		
