f8d80c6f6a5a99ecbb7b88071bfee372a079a50a
League <league@iMac-3.local>
2019-06-16 17:30:27 -0700

    start base 64 decoder

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,32 +6,47 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
-		byte firstBit = (byte) (b & 1);
-		byte secondBit = (byte) ((b & 2) >> 1);
-		byte thirdBit = (byte) ((b & 4) >> 2);
-		byte fourthBit = (byte) ((b & 8) >> 3);
-		byte fifthBit = (byte) ((b & 16) >> 4);
-		byte sixthBit = (byte) ((b & 32) >> 5);
-		byte seventhBit = (byte) ((b & 64) >> 6);
 		byte eighthBit = (byte) ((b & 128) >> 7);
+		byte seventhBit = (byte) ((b & 64) >> 6);
+		byte sixthBit = (byte) ((b & 32) >> 5);
+		byte fifthBit = (byte) ((b & 16) >> 4);
+		byte fourthBit = (byte) ((b & 8) >> 3);
+		byte thirdBit = (byte) ((b & 4) >> 2);
+		byte secondBit = (byte) ((b & 2) >> 1);
+		byte firstBit = (byte) (b & 1);
+		
 		String binary = "";
 		
-		binary = "" + firstBit + ""+ secondBit + "" + thirdBit + "" + fourthBit + "" + fifthBit + "" + sixthBit + ""+ seventhBit +  "" + eighthBit;
+		binary = "" + eighthBit + ""+ seventhBit + "" + sixthBit + "" + fifthBit + "" + fourthBit + "" + thirdBit + ""+ secondBit +  "" + firstBit;
 		System.out.print(binary);
 	}
 		public static void main(String[] args) {
 			BinaryPrinter bp = new BinaryPrinter();
 			bp.printByteBinary((byte)129);
 			System.out.println();
+			System.out.println(Integer.toBinaryString(129));
+			System.out.println();
+			
 			bp.printShortBinary((short)300);
 			System.out.println();
-			bp.printIntBinary((int)123456);
+			System.out.println(Integer.toBinaryString(300));
+			System.out.println();
+			
+			bp.printIntBinary((int)223456);
+			System.out.println();
+			System.out.println(Integer.toBinaryString(223456));
+			System.out.println();
+			
+			bp.printLongBinary((long)923456768);
+			System.out.println();
+			System.out.println(Integer.toBinaryString(923456768));
+			System.out.println();
 			
 		}
 	
 	public void printShortBinary(short s) {
-		byte firstByte = (byte) (s >> 8);
-		byte secondByte =(byte) (s&255);
+		byte firstByte = (byte) ((s & 0xFF00) >> 8);
+		byte secondByte =(byte) ((s & 0x00FF) >> 0);
 		printByteBinary(firstByte);
 		printByteBinary(secondByte);
 	}
@@ -39,13 +54,17 @@ public class BinaryPrinter {
 	public void printIntBinary(int i) {
 		//4bytes
 		short firstShort = (short) (i >> 16);
-		short secondShort = (short) (i & 32767);	
+		short secondShort = (short) (i & 65535);	
 		printShortBinary(firstShort);
 		printShortBinary(secondShort);
 	}
 //	8 128 9 256 10 512 11 1024 12 2048 13 4096 14 8192 15 16384 16 32768 
-	//17 65536 18 131072 19 262144 20 524288 21 1048576
+	//17 65536 18 131072 19 262144 20 524288 21 1048576 22 2097152     30 268435456    32 1073741824
 	public void printLongBinary(long l) {
 		//8 bytes
+		int firstInt = (int) (l >> 32);
+		int secondInt = (int) (l & 1073741823);
+		printIntBinary(firstInt);
+		printIntBinary(secondInt);
 	}
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -30,26 +30,48 @@ public class Base64Decoder {
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'/*48*/, 'x', 'y', 'z',
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
 	};
 	
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		int index = 0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if(base64Chars[i] == c) {
+				index = i;
+			}
+		}
+		return (byte) index;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		return null;
+		byte[] arr = new byte[3];
+		byte a = convertBase64Char(s.charAt(0));
+		byte b = convertBase64Char(s.charAt(1));
+		byte c = convertBase64Char(s.charAt(2));
+		byte d = convertBase64Char(s.charAt(3));
+//		a = (byte) (a >>2);
+		arr[0] = (byte) ((byte) (a<<2) | (b>>4));
+		arr[1] = (byte) ((byte) (b<<4) | (c>>2));
+		arr[2] = (byte) ((byte) (c<<6) | d);
+		return arr;
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
 	//   and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
-		return null;
+		byte[] arr = new byte[file.length()];
+		for (int i = 0; i < base64Chars.length; i++) {
+			if(base64Chars[i] == file.charAt(i)) {
+				arr[i] = (byte) base64Chars[i];
+			}
+			
+		}
+		return arr;
 	}
 }
